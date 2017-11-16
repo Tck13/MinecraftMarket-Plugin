80f9ae6d0e60cbac4f6deab074fc7ee21c6e9d49
Rui Pereira <r4g3baby@gmail.com>
2017-11-16 17:19:00 +0000

    Updated plugin version

--- sponge/src/main/java/com/minecraftmarket/minecraftmarket/sponge/MCMarket.java
+++ sponge/src/main/java/com/minecraftmarket/minecraftmarket/sponge/MCMarket.java
@@ -36,7 +36,7 @@ import java.util.Optional;
 @Plugin(
         id = "minecraftmarket",
         name = "MinecraftMarket",
-        version = "3.5.1",
+        version = "3.5.2",
         description = "The #1 webstore platform for Minecraft servers",
         authors = "R4G3_BABY",
         url = "https://www.minecraftmarket.com"
